be390a67656e6bb6ae4024a7821e282d59376656
Mikhail <mikhail@volkovlabs.io>
2023-05-21 19:35:43 -0400

    Update changelog

--- src/components/AbcPanel/AbcPanel.test.tsx
+++ src/components/AbcPanel/AbcPanel.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { toDataFrame } from '@grafana/data';
+import { render, screen } from '@testing-library/react';
 import { TestIds } from '../../constants';
 import { AbcPanel } from './AbcPanel';
 
